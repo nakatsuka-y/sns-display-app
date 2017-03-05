33c51aad3c622a164434137b94b704110535d907
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2017-03-05 20:06:25 +0900

    successfully obtained request token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  require 'uri'
+  require 'cgi'
+
+  include AuthHelper
+
   def home
-    render html: '<a href="#">Log in and view Twitter timeline</a>'
+    # Display twitter login link
+	login_url = get_login_url
+	#get_login_url
+    render html: "<a href='#{login_url}'>Log in and view Twitter timeline</a>".html_safe
+  end
+
+  def authorize
+	params = CGI.parse(URI.parse(request.original_url).query)
+    token = prepare_access_token(params["oauth_verifier"]
+	#response = token.request(:get, "https://api.twitter.com/1.1/statuses/home_timeline.json")
+	#render html: "#{response}".html_safe
   end
 end
---
+++ app/helpers/auth_helper.rb
@@ -0,0 +1,31 @@
+module AuthHelper
+  CLIENT_ID = "oikQMCs5TAL9oDhjmlZSBzHlu"
+  CLIENT_SECRET = "3HXmXL4Dql6Z70ap7jnEj8FGkEzdVjxCyOxYOIiC6MB4nd6jOg"
+  REDIRECT_URI = "http://127.0.0.1:3000/authorize"
+
+  def get_login_url
+	client = OAuth::Consumer.new(CLIENT_ID,
+								 CLIENT_SECRET,
+								 {:site => "https://api.twitter.com",
+								  :authorize_path => "/oauth/authorize",
+								  :request_token_path => "/oauth/request_token"})
+	request_token = client.get_request_token({:oauth_callback => REDIRECT_URI})
+	session[:request_token] = request_token.token
+	session[:request_token_secret] = request_token.secret
+
+	login_url = request_token.authorize_url(:oauth_callback => REDIRECT_URI)
+	#redirect_to request_token.authorize_url(:oauth_callback => REDIRECT_URI)
+  end
+
+  def prepare_access_token(oauth_verifier)
+	client = OAuth::Consumer.new(CLIENT_ID,
+								 CLIENT_SECRET,
+								 {:site => "https://api.twitter.com",
+								  :authorize_path => "/oauth/authorize",
+								  :access_token_path => "/oauth/access_token"})
+	access_token = client.get_access_token({:oauth_callback => REDIRECT_URI})
+	token_hash = { :oauth_token => access_token.token, :oauth_token_secret => access_token.secret }
+	request_token = OAuth::RequestToken.from_hash(client, token_hash)
+	return request_token
+  end
+end
